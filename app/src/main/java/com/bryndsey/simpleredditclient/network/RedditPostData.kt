b1d64f42f6d050aae5ca31a982edd93aa46f95b1
Unknown <blinz117@gmail.com>
2017-11-19 09:03:44 -0600

    Add comment display
    Log the post URL when the comments are clicked

--- app/src/main/java/com/bryndsey/simpleredditclient/network/RedditPostData.kt
+++ app/src/main/java/com/bryndsey/simpleredditclient/network/RedditPostData.kt
@@ -6,4 +6,6 @@ import com.google.gson.annotations.SerializedName
 data class RedditPostData(
         @SerializedName("title") val title: String?,
         @SerializedName("selftext") val text: String?,
-        @SerializedName("score") val score: Int?)
+        @SerializedName("score") val score: Int?,
+        @SerializedName("num_comments") val numComments: Int?,
+        @SerializedName("url") val url: String?)
--- app/src/main/java/com/bryndsey/simpleredditclient/ui/RedditPostViewHolder.kt
+++ app/src/main/java/com/bryndsey/simpleredditclient/ui/RedditPostViewHolder.kt
@@ -1,6 +1,7 @@
 package com.bryndsey.simpleredditclient.ui
 
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.View
 import com.bryndsey.simpleredditclient.network.RedditPostData
 import kotlinx.android.synthetic.main.reddit_post.view.*
@@ -10,7 +11,11 @@ class RedditPostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun bind(redditPostData: RedditPostData) {
         itemView.reddit_post_title.text = redditPostData.title
-        Markwon.setMarkdown(itemView.reddit_post_text, redditPostData.text.orEmpty())
         itemView.reddit_post_score.text = redditPostData.score.toString()
+        itemView.reddit_post_comments.text = redditPostData.numComments.toString() + " comments"
+        itemView.reddit_post_comments.setOnClickListener { view ->
+            Log.d("BRYAN", "Clicked comments. Opening url " + redditPostData.url)
+        }
+        Markwon.setMarkdown(itemView.reddit_post_text, redditPostData.text.orEmpty())
     }
 }
