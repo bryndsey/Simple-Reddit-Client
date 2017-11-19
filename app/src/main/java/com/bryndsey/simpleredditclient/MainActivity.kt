344da39a93f6a341ee03435c022217cdfafc2568
Unknown <blinz117@gmail.com>
2017-11-19 09:27:18 -0600

    Add basic retry and error handling to post fetching

--- app/src/main/java/com/bryndsey/simpleredditclient/MainActivity.kt
+++ app/src/main/java/com/bryndsey/simpleredditclient/MainActivity.kt
@@ -3,8 +3,9 @@ package com.bryndsey.simpleredditclient
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.util.Log
+import android.widget.Toast
 import com.bryndsey.simpleredditclient.network.RedditPost
+import com.bryndsey.simpleredditclient.network.RedditPostData
 import com.bryndsey.simpleredditclient.network.RedditService
 import com.bryndsey.simpleredditclient.ui.RedditPostAdapter
 import io.reactivex.Observable
@@ -16,11 +17,13 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var adapter: RedditPostAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val adapter = RedditPostAdapter()
+        adapter = RedditPostAdapter()
         post_list.adapter = adapter
         post_list.layoutManager = LinearLayoutManager(this)
 
@@ -45,16 +48,19 @@ class MainActivity : AppCompatActivity() {
         }
 
         postObservable
+                .retry()
                 .flatMap { list -> Observable.fromIterable(list) }
                 .map { redditPost -> redditPost.data }
                 .toList()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe{ postList ->
-                    run {
-                        adapter.postList = postList
-                        adapter.notifyDataSetChanged()
-                    }
-                }
+                .subscribe({ postList -> updatePosts(postList)},
+                        { error -> Toast.makeText(this, "Error occurred fetching posts", Toast.LENGTH_SHORT).show() }
+        )
+
     }
+        private fun updatePosts(postList: List<RedditPostData>) {
+            adapter.postList = postList
+            adapter.notifyDataSetChanged()
+        }
 }
