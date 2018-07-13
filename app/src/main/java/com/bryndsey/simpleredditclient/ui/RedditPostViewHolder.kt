121108d4688cf4e08db1a96e3f5081fc7c5d97ce
Unknown <blinz117@gmail.com>
2018-07-13 07:06:28 -0500

    Use with block to reduce verbosity

--- app/src/main/java/com/bryndsey/simpleredditclient/ui/RedditPostViewHolder.kt
+++ app/src/main/java/com/bryndsey/simpleredditclient/ui/RedditPostViewHolder.kt
@@ -10,19 +10,25 @@ import ru.noties.markwon.Markwon
 class RedditPostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun bind(redditPostData: RedditPostData) {
-        itemView.reddit_post_title.text = redditPostData.title
-        itemView.reddit_post_score.text = redditPostData.score.toString()
-        itemView.reddit_post_comments.text = redditPostData.numComments.toString() + " comments"
-        itemView.reddit_post_comments.setOnClickListener {
-            Log.d("BRYAN", "Clicked comments. Opening url " + redditPostData.url)
-        }
-        Markwon.setMarkdown(itemView.reddit_post_text, redditPostData.text.orEmpty())
-        itemView.reddit_post_text.visibility = View.GONE
-        itemView.setOnClickListener {
-            if (itemView.reddit_post_text.visibility == View.VISIBLE) {
-                itemView.reddit_post_text.visibility = View.GONE
-            } else {
-                itemView.reddit_post_text.visibility = View.VISIBLE
+        with(itemView) {
+            reddit_post_title.text = redditPostData.title
+            reddit_post_score.text = redditPostData.score.toString()
+            reddit_post_comments.text = redditPostData.numComments.toString() + " comments"
+
+            reddit_post_comments.setOnClickListener {
+                Log.d("BRYAN", "Clicked comments. Opening url " + redditPostData.url)
+            }
+
+            Markwon.setMarkdown(reddit_post_text, redditPostData.text.orEmpty())
+
+            reddit_post_text.visibility = View.GONE
+
+            setOnClickListener {
+                if (reddit_post_text.visibility == View.VISIBLE) {
+                    reddit_post_text.visibility = View.GONE
+                } else {
+                    reddit_post_text.visibility = View.VISIBLE
+                }
             }
         }
     }
