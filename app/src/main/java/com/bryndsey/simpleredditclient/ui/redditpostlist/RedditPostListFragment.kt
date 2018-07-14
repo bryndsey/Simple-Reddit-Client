5be7fc5f16fb3e491612ba60837917c408ede63f
Unknown <blinz117@gmail.com>
2018-07-14 15:14:27 -0500

    Add viewModel for list fragment
    In this process, I also implemented some simple caching within the viewModel so that it doesn't need to refetch data from the repository if the repository hasn't had anything changed

--- app/src/main/java/com/bryndsey/simpleredditclient/ui/redditpostlist/RedditPostListFragment.kt
+++ app/src/main/java/com/bryndsey/simpleredditclient/ui/redditpostlist/RedditPostListFragment.kt
@@ -1,5 +1,6 @@
 package com.bryndsey.simpleredditclient.ui.redditpostlist
 
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -7,8 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.bryndsey.simpleredditclient.R
-import com.bryndsey.simpleredditclient.data.RedditPostRepository
 import com.bryndsey.simpleredditclient.di.ComponentHolder
+import com.bryndsey.simpleredditclient.di.ViewModelFactory
 import com.bryndsey.simpleredditclient.network.RedditPostData
 import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.reddit_post_list.*
@@ -18,11 +19,15 @@ class RedditPostListFragment: Fragment() {
 
     private lateinit var adapter: RedditPostListAdapter
 
-    @Inject lateinit var redditPostRepository: RedditPostRepository
+    @Inject lateinit var viewModelFactory: ViewModelFactory<RedditPostListViewModel>
+    private lateinit var viewModel: RedditPostListViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ComponentHolder.applicationComponent.inject(this)
+
+        viewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(RedditPostListViewModel::class.java)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -35,7 +40,7 @@ class RedditPostListFragment: Fragment() {
         adapter = RedditPostListAdapter()
         post_list.adapter = adapter
 
-        redditPostRepository.fetchRedditPosts()
+        viewModel.getRedditPosts()
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({ postList -> updatePosts(postList) },
                         { Toast.makeText(context, "Error occurred fetching posts", Toast.LENGTH_SHORT).show() }
