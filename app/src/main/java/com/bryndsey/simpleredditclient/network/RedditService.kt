4495efa6c1ee7582ace5aedc33cd3874d5a5f986
Unknown <blinz117@gmail.com>
2018-09-02 17:00:08 -0500

    Rename remaining API models to include "Api" prefix
    This helps makes their purpose explicit so that they (hopefully) aren't used in the rest of the app any more in the future

--- app/src/main/java/com/bryndsey/simpleredditclient/network/RedditService.kt
+++ app/src/main/java/com/bryndsey/simpleredditclient/network/RedditService.kt
@@ -6,5 +6,5 @@ import retrofit2.http.GET
 interface RedditService {
 
     @GET("r/AndroidDev/.json")
-    fun getSubredditPosts(): Single<RedditResponse>
+    fun getSubredditPosts(): Single<ApiRedditResponse>
 }
