8af3e131f6202c67417e858d4f4c8ad664b98c8f
Unknown <blinz117@gmail.com>
2018-09-02 16:44:20 -0500

    Add a new class called ApiRedditPost
    This will eventually be the model used to represent the API data, and the existing model will be the one used throughout the app. This will help isolate the rest of the app from changes to the API model.

---
+++ app/src/main/java/com/bryndsey/simpleredditclient/network/ApiRedditPostData.kt
@@ -0,0 +1,14 @@
+package com.bryndsey.simpleredditclient.network
+
+import com.google.gson.annotations.SerializedName
+
+
+data class ApiRedditPostData(
+        @SerializedName("title") val title: String?,
+        @SerializedName("selftext") val text: String?,
+        @SerializedName("score") val score: Int?,
+        @SerializedName("num_comments") val numComments: Int?,
+        @SerializedName("url") val url: String?,
+        @SerializedName("id") val id: String,
+        @SerializedName("is_self") val isSelf: Boolean
+)
