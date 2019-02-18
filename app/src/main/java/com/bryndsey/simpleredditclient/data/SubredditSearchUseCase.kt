a53b9ef533411357738153e57ed8e336d40a402d
Bryan Lindsey <blinz117@gmail.com>
2019-02-17 20:24:33 -0600

    Ensure subscription on a background thread

--- app/src/main/java/com/bryndsey/simpleredditclient/data/SubredditSearchUseCase.kt
+++ app/src/main/java/com/bryndsey/simpleredditclient/data/SubredditSearchUseCase.kt
@@ -3,12 +3,14 @@ package com.bryndsey.simpleredditclient.data
 import com.bryndsey.simpleredditclient.network.RedditService
 import com.bryndsey.simpleredditclient.network.toSubreddit
 import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class SubredditSearchUseCase @Inject constructor(val redditService: RedditService) {
 
     fun searchSubreddits(searchTerm: String): Single<List<Subreddit>> {
         return redditService.searchSubreddits(searchTerm)
+                .subscribeOn(Schedulers.io())
                 .map { subredditList ->
                     subredditList.data.posts.map {
                         it.data.toSubreddit()
