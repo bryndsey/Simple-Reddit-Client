56da3259738dcbfeda332c4201a1349b04bca37a
Bryan Lindsey <blinz117@gmail.com>
2019-02-18 21:54:58 -0600

    Add ViewModel for subreddit search screen
    This helps with separation of concern, but mainly serves to make sure that the state is restored across configuration changes.

--- app/src/main/java/com/bryndsey/simpleredditclient/ui/subredditselection/SubredditSearchFragment.kt
+++ app/src/main/java/com/bryndsey/simpleredditclient/ui/subredditselection/SubredditSearchFragment.kt
@@ -1,28 +1,31 @@
 package com.bryndsey.simpleredditclient.ui.subredditselection
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.annotation.ContentView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.bryndsey.simpleredditclient.R
-import com.bryndsey.simpleredditclient.data.SubredditSearchUseCase
 import com.bryndsey.simpleredditclient.di.ComponentHolder
+import com.bryndsey.simpleredditclient.di.ViewModelFactory
 import com.bryndsey.simpleredditclient.ui.BaseFragment
-import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.subreddit_search_view.*
 import javax.inject.Inject
 
 @ContentView(R.layout.subreddit_search_view)
 class SubredditSearchFragment : BaseFragment() {
 
-    @Inject
-    lateinit var subredditSearchUseCase: SubredditSearchUseCase
+    @Inject lateinit var viewModelFactory: ViewModelFactory<SubredditSearchViewModel>
+
+    private lateinit var viewModel: SubredditSearchViewModel
 
     private val subredditSearchAdapter = SubredditOverviewAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ComponentHolder.applicationComponent.inject(this)
+
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(SubredditSearchViewModel::class.java)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -30,17 +33,15 @@ class SubredditSearchFragment : BaseFragment() {
 
         subredditSearchResultsRecyclerView.adapter = subredditSearchAdapter
 
+        viewModel.searchLiveData.observe(this, Observer{
+            subredditSearchAdapter.setAdapterData(it)
+        })
+
         subredditPerformSearchButton.setOnClickListener { performSearch() }
     }
 
     private fun performSearch() {
         val searchTerm = subredditNameSearchBox.text.toString()
-        val disposable = subredditSearchUseCase.searchSubreddits(searchTerm)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        { subredditSearchAdapter.setAdapterData(it) },
-                        { Log.e("BRYAN", "error occurred searching for subreddits. Try again later.", it) })
-
-        addSubscription(disposable)
+        viewModel.performSearch(searchTerm)
     }
 }
