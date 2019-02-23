73c56a35de88c268630a8549f6bd3f9b8839c6a6
Bryan Lindsey <blinz117@gmail.com>
2019-02-23 08:05:11 -0600

    Add more convenience time constants

--- app/src/main/java/com/bryndsey/simpleredditclient/data/TimeConstants.kt
+++ app/src/main/java/com/bryndsey/simpleredditclient/data/TimeConstants.kt
@@ -6,4 +6,9 @@ object TimeConstants {
     const val MINUTES_PER_HOUR = 60L
     const val HOURS_PER_DAY = 24L
     const val DAYS_PER_WEEK = 7L
+
+    const val MILLIS_PER_MINUTE = MILLIS_PER_SECOND * SECONDS_PER_MINUTE
+    const val MILLIS_PER_HOUR = MILLIS_PER_MINUTE * MINUTES_PER_HOUR
+    const val MILLIS_PER_DAY = MILLIS_PER_HOUR * HOURS_PER_DAY
+    const val MILLIS_PER_WEEK = MILLIS_PER_DAY * DAYS_PER_WEEK
 }
