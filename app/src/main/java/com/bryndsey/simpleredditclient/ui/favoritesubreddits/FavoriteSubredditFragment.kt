42101c6497f3120ca1c7f5bcf3d16153d945f555
Bryan Lindsey <blinz117@gmail.com>
2019-02-23 21:26:50 -0600

    Add a quick and dirty favorite subreddit view
    Right now, it just shows a lit of hardcoded subreddits, but it works with going to a post list. It will certainly be good for quickly accessing subreddits during development (so I don't have to type them in every time).

---
+++ app/src/main/java/com/bryndsey/simpleredditclient/ui/favoritesubreddits/FavoriteSubredditFragment.kt
@@ -0,0 +1,29 @@
+package com.bryndsey.simpleredditclient.ui.favoritesubreddits
+
+import android.os.Bundle
+import android.view.View
+import androidx.annotation.ContentView
+import com.bryndsey.simpleredditclient.R
+import com.bryndsey.simpleredditclient.data.Subreddit
+import com.bryndsey.simpleredditclient.ui.BaseFragment
+import com.bryndsey.simpleredditclient.ui.subredditselection.SubredditOverviewAdapter
+import kotlinx.android.synthetic.main.favorite_subreddit_view.*
+
+@ContentView(R.layout.favorite_subreddit_view)
+class FavoriteSubredditFragment : BaseFragment() {
+
+    val testFavoriteSubredditList = listOf<Subreddit>(
+            Subreddit("AndroidDev", "r/AndroidDev", "Fake description", 100),
+            Subreddit("DadJokes", "r/DadJokes", "Fake description", 101),
+            Subreddit("Gaming", "r/Gaming", "Fake description", 102)
+    )
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        favoriteSubredditRecyclerView.adapter = SubredditOverviewAdapter().apply {
+            setAdapterData(testFavoriteSubredditList)
+        }
+
+    }
+}
