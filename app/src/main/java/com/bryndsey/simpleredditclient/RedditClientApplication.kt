57176eb8f4c20fe5db2c37b5be446dc4a21392c7
Bryan Lindsey <blinz117@gmail.com>
2019-03-12 21:42:18 -0500

    Start Koin from the application

--- app/src/main/java/com/bryndsey/simpleredditclient/RedditClientApplication.kt
+++ app/src/main/java/com/bryndsey/simpleredditclient/RedditClientApplication.kt
@@ -1,9 +1,8 @@
 package com.bryndsey.simpleredditclient
 
 import android.app.Application
-import com.bryndsey.simpleredditclient.di.ComponentHolder
-import com.bryndsey.simpleredditclient.di.DaggerApplicationComponent
-import com.bryndsey.simpleredditclient.di.NetworkModule
+import com.bryndsey.simpleredditclient.di.*
+import org.koin.android.ext.android.startKoin
 
 class RedditClientApplication : Application() {
 
@@ -13,5 +12,10 @@ class RedditClientApplication : Application() {
         ComponentHolder.applicationComponent = DaggerApplicationComponent.builder()
                 .networkModule(NetworkModule)
                 .build()
+
+        startKoin(
+                this,
+                listOf(networkModule, dataModule, viewModelModule)
+        )
     }
 }
