5d0cbe8f23a11d46e23864ada5fb028fb1c2673d
Bryan Lindsey <blinz117@gmail.com>
2019-03-15 09:03:15 -0500

    Fix boundary case for large number display format

--- app/src/main/java/com/bryndsey/simpleredditclient/ui/UiExtensions.kt
+++ app/src/main/java/com/bryndsey/simpleredditclient/ui/UiExtensions.kt
@@ -1,8 +1,7 @@
 package com.bryndsey.simpleredditclient.ui
 
 fun Int.toDisplayString(): String {
-    // FIXME: This should use >=
-    return if (this > 1000) {
+    return if (this >= 1000) {
         val numberInThousands = this / 1000F
         "%.1fK".format(numberInThousands)
     } else {
