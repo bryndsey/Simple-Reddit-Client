1a7f3c209d2930215ff60103b4bbd0462ef0e7dc
Bryan Lindsey <blinz117@gmail.com>
2019-03-21 17:02:45 -0500

    Add "after" query param for fetching posts
    This can be used to get subsequent "pages". Passing null will simply omit that param, and also use null by default (which works out for the existing initial fetch)

--- app/src/main/java/com/bryndsey/simpleredditclient/network/RedditService.kt
+++ app/src/main/java/com/bryndsey/simpleredditclient/network/RedditService.kt
@@ -8,7 +8,9 @@ import retrofit2.http.Query
 interface RedditService {
 
     @GET("r/{subredditName}/.json")
-    fun getSubredditPosts(@Path("subredditName") subredditName: String): Single<ApiRedditResponse>
+    fun getSubredditPosts(
+            @Path("subredditName") subredditName: String,
+            @Query("after") lastFetchedPostFullname: String? = null): Single<ApiRedditResponse>
 
     @GET("subreddits/search.json")
     fun searchSubreddits(@Query("q") subredditName: String): Single<ApiRedditResponse>
