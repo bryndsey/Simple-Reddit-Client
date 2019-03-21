3ee7084574789bdd38dacf8091c00841a6c45ea5
Bryan Lindsey <blinz117@gmail.com>
2019-03-21 17:32:17 -0500

    Fetch more posts if user is near end of the list
    This code detects if the user is close to scrolling to the end of the post list, and if so tells the ViewModel to fetch more.
    
    There is some commentary in here wondering if this is the best solution or not. For now, it works, so we'll go with it.
    
    Note that I also removed the layout manager tag from the XML and instantiated it in the fragment instead. While this is generally not my preference, the fragment was relying on the use of a LinearLayoutManager anyway to detect the last item, so I figured I might as well make that implementation detail encapsulated in the Fragment rather than having the Fragment coupled to details in the XML (which if changed could end up breaking things).

--- app/src/main/java/com/bryndsey/simpleredditclient/ui/redditpostlist/RedditPostListFragment.kt
+++ app/src/main/java/com/bryndsey/simpleredditclient/ui/redditpostlist/RedditPostListFragment.kt
@@ -6,10 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bryndsey.simpleredditclient.R
 import com.bryndsey.simpleredditclient.data.RedditPost
 import kotlinx.android.synthetic.main.reddit_post_list.*
 import org.koin.android.viewmodel.ext.android.viewModel
+
+private const val reloadBufferThreshold = 4
 
 class RedditPostListFragment: Fragment() {
 
@@ -29,6 +33,28 @@ class RedditPostListFragment: Fragment() {
         postListViewModel.initialize(subredditName)
 
         post_list.adapter = adapter
+
+        val layoutManager = LinearLayoutManager(context)
+        post_list.layoutManager = layoutManager
+        
+        // TODO: This could potentially be replaced with Paging library stuff
+        // but it seemed overly complicated when investigating, so I just rolled my own stuff
+        post_list.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                // If we're almost at the bottom (i.e. the index of last visible item is close to
+                // the last index within a certain threshold), then attempt to load more items
+                //
+                // NOTE: This is not at all optimized, and it very much assumes that the ViewModel
+                // will ignore request spamming appropriately. I'm not really sure if this is a bad
+                // idea long term or not.
+                val newLastPositionIndex = layoutManager.findLastVisibleItemPosition()
+                if (newLastPositionIndex > layoutManager.itemCount - reloadBufferThreshold) {
+                    postListViewModel.loadMorePosts()
+                }
+            }
+        })
 
         postListViewModel.postListLiveData.observe(viewLifecycleOwner, Observer {
             updatePosts(it)
