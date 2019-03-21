d007277770ee235d5a47f2b8837bf418f0b5b4a3
Bryan Lindsey <blinz117@gmail.com>
2019-03-21 17:12:41 -0500

    Add ViewModel method for fetching more posts
    To achieve this, the ViewModel holds a cached version of the full post list. I'm ultimately not sure if this makes more sense to do in the repository somehow, but I ended up going this route because this helped with resetting the cached list if the user navigated out of the scope of the ViewModel.
    
    Like in the previous commit, there is some duplicated logic which should be cleaned up eventually, but I'm more interested in getting everything working first and then cleaning up later - hence the FIXME comment.

--- app/src/main/java/com/bryndsey/simpleredditclient/ui/redditpostlist/RedditPostListViewModel.kt
+++ app/src/main/java/com/bryndsey/simpleredditclient/ui/redditpostlist/RedditPostListViewModel.kt
@@ -12,18 +12,25 @@ class RedditPostListViewModel(private val redditPostRepository: RedditPostReposi
         ViewModel() {
 
     private var postFetchDisposable: Disposable? = null
+    private var morePostFetchDisposable: Disposable? = null
 
     private val _postListLiveData = MutableLiveData<List<RedditPost>>()
+
+    private lateinit var subredditName: String
+    private lateinit var postList : MutableList<RedditPost>
 
     val postListLiveData: LiveData<List<RedditPost>>
         get() = _postListLiveData
 
     fun initialize(subredditName: String) {
         if (postFetchDisposable == null) {
+            this.subredditName = subredditName
             postFetchDisposable = redditPostRepository.fetchRedditPosts(subredditName)
                     .retry(2)
                     .onErrorReturn { emptyList() }
                     .subscribe({
+                        postList = it.toMutableList()
+                        // Post immutable list so future updates won't mess with things
                         _postListLiveData.postValue(it)
                     }, {
                         // TODO: Show an error state when this occurs
@@ -32,9 +39,34 @@ class RedditPostListViewModel(private val redditPostRepository: RedditPostReposi
         }
     }
 
+    fun loadMorePosts() {
+        // Prevent attempts to load more if we're already in the middle of loading
+        if (morePostFetchDisposable != null) return
+
+        val lastPostFullname = postList.last().fullname
+        morePostFetchDisposable = redditPostRepository.fetchMoreRedditPosts(subredditName, lastPostFullname)
+                // FIXME: A lot of this is largely duplicated from the initial fetch
+                .retry(2)
+                .onErrorReturn { emptyList() }
+                .doAfterTerminate {
+                    // Reset disposable so we know it's safe to fetch more posts
+                    // Note that the disposable should already be disposed by this point
+                    morePostFetchDisposable = null
+                }
+                .subscribe({
+                    postList.addAll(it)
+                    _postListLiveData.postValue(postList.toList())
+                }, {
+                    // TODO: Show an error state when this occurs
+                    Log.e("BRYAN", "Error fetching more posts for subreddit $subredditName", it)
+                })
+    }
+
     override fun onCleared() {
         postFetchDisposable?.dispose()
         postFetchDisposable = null
+
+        morePostFetchDisposable?.dispose()
 
         super.onCleared()
     }
