186e4c5fd57dbaebcdcd4e833b5483148d6d53d6
Bryan Lindsey <blinz117@gmail.com>
2019-03-21 17:07:56 -0500

    Add repository method for fetching more posts
    It ends up duplicating stuff from the other method, so I'd eventually like to find a way to clean that up. But for now, I'd rather get the whole thing working first.

--- app/src/main/java/com/bryndsey/simpleredditclient/data/RedditPostRepository.kt
+++ app/src/main/java/com/bryndsey/simpleredditclient/data/RedditPostRepository.kt
@@ -22,6 +22,19 @@ class RedditPostRepository(private val redditService: RedditService) {
                 .subscribeOn(Schedulers.io())
     }
 
+    fun fetchMoreRedditPosts(subredditName: String, lastPostId: String): Single<List<RedditPost>> {
+        return redditService.getSubredditPosts(subredditName, lastPostId)
+                // TODO: The rest of this is duplicated. Find a better way to reuse the logic
+                .retry()
+                .map {
+                    it.data.posts.map { post -> post.data.toRedditPost() }
+                }
+                .doOnSuccess {
+                    postDataSet.addAll(it)
+                }
+                .subscribeOn(Schedulers.io())
+    }
+
     fun getPostById(id: String?): Maybe<RedditPost> {
         val post = postDataSet.find { it.id == id }
 
