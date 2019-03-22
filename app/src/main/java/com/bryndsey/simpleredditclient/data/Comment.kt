f1a8922c8ea6a68a2183b32286cd12b78e3d54da
Bryan Lindsey <blinz117@gmail.com>
2019-03-22 15:57:27 -0500

    Add ability for comment to have children

--- app/src/main/java/com/bryndsey/simpleredditclient/data/Comment.kt
+++ app/src/main/java/com/bryndsey/simpleredditclient/data/Comment.kt
@@ -2,5 +2,6 @@ package com.bryndsey.simpleredditclient.data
 
 data class Comment(
         val text: String,
-        val score: Int
+        val score: Int,
+        val childComments: List<Comment> = emptyList()
 )
